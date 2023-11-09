deaa1d4bba456227bf01125f52384904266b6d40
thiagomatos <34483743+thiagombrasil@users.noreply.github.com>
2023-11-09 03:29:29 +0000

    Cria classe Quarto para representar ambiente estatico

--- AgenteRacional.java
+++ AgenteRacional.java
@@ -1,3 +1,34 @@
 public class AgenteRacional {
+    
+    private static class Quarto {
+        char[][] ambiente;
+        char[][] estadoQuarto;
 
+        Quarto(char[][] ambiente, char[] pontosSujos) {
+            this.ambiente = ambiente;
+            this.estadoQuarto = new char[ambiente.length][ambiente[0].length];
+            this.inicializarEstadoQuarto(pontosSujos);
+        }
+
+        private void inicializarEstadoQuarto(char[] pontosSujos) {
+            for (int i = 0; i < estadoQuarto.length; i++) {
+                for (int j = 0; j < estadoQuarto[i].length; j++) {
+                    if (this.contemPontoSujos(pontosSujos, ambiente[i][j])) {
+                        estadoQuarto[i][j] = 'S';
+                    } else {
+                        estadoQuarto[i][j] = 'L';
+                    }
+                }
+            }
+        }
+
+        private static boolean contemPontoSujos(char[] pontosSujos, char ponto) {
+            for (char sujeira : pontosSujos) {
+                if (sujeira == ponto) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
